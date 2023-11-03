47926cd230925200f4a933c74be7776063bddfab
lin.hanchao <lin.hanchao@byd.com>
2023-11-03 08:45:55 +0800

    ✨ feat: 更新请求响应内容格式化方法

--- lib/src/request.dart
+++ lib/src/request.dart
@@ -1,9 +1,11 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 
 class Request {
   static String _domain = 'fishpi.cn';
   static String _protocol = 'https';
-  static Future<T> get<T>(String url, { Map<String, dynamic>? params }) async {
+  static Future<T> get<T>(String url, {Map<String, dynamic>? params}) async {
     if (params != null) {
       url = '$url?';
       params.forEach((key, value) {
@@ -21,14 +23,18 @@ class Request {
   static Future<T> request<T>(String url, {method, data}) async {
     try {
       var dio = Dio();
-      var response =
-          await dio.request('$_protocol://$_domain/$url', data: data, options: Options(method: method));
+      var response = await dio.request('$_protocol://$_domain/$url',
+          data: data, options: Options(method: method));
       if (response.statusCode == 200 || response.statusCode == 201) {
         try {
           if (response.data is Map) {
             return response.data;
           } else {
-            return response.data;
+            try {
+              return json.decode(response.data);
+            } catch (e) {
+              return response.data;
+            }
           }
         } catch (e) {
           return Future.error('解析响应数据异常');
@@ -44,9 +50,7 @@ class Request {
   }
 
   static Future<FormData> formData(String key,
-      {Map<String, dynamic>? src,
-      List<String>? files,
-      String? value}) async {
+      {Map<String, dynamic>? src, List<String>? files, String? value}) async {
     src ??= {};
     if (files != null) {
       src[key] = await Future.wait(files.map((filePath) async {
