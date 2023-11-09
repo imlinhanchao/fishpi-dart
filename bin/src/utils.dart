92e2ffbe69f870e80b155575f8ed4b45cf8cce2e
Hancel.Lin <imlinhanchao@gmail.com>
2023-11-09 16:30:00 +0800

    ✨ feat: 添加服务器切换命令保存配置

--- bin/src/utils.dart
+++ bin/src/utils.dart
@@ -9,17 +9,18 @@ class UtilsCmd implements CommandInstance {
     return parser
       ..addOption('upload', abbr: 'f', help: '上传文件路径')
       ..addOption('origin',
-          abbr: 'o', help: '服务器地址', defaultsTo: 'https://fishpi.cn');
+          abbr: 'o', help: '服务器地址， 默认为 https://fishpi.cn');
   }
 
   @override
   Future<void> exec(ArgResults args, PrintFn print) async {
     var upload = args['upload'];
-    var origin = args['origin'];
+    var origin = args['origin'] ?? Instance.cfg.config['origin'];
 
     if (origin != null) {
-      Fishpi.setOrigin(args['origin']);
+      Fishpi.setOrigin(origin);
       Instance.cfg.set('origin', origin);
+      Instance.cfg.save();
     }
 
     if (upload != null) {
